c46ad293c0fefc3525d024c2af3d1710013f5eb8
Tharunkannan <teekay1810@gmail.com>
2025-03-03 21:15:42 +0000

    both vertical and horizontal movements work very well guys

--- sprites.py
+++ sprites.py
@@ -12,7 +12,10 @@ class MovingSprite(Sprite):
     def __init__(self,groups, start_pos , end_pos, move_dir, speed):
         surf = pygame.Surface((200,50))
         super().__init__(start_pos, surf, groups)
-        self.rect.center = start_pos
+        if move_dir == 'x':
+            self.rect.midleft = start_pos
+        else:
+            self.rect.midtop = start_pos
         self.start_pos = start_pos
         self.end_pos = end_pos
         speed = 100   
@@ -23,14 +26,20 @@ class MovingSprite(Sprite):
         self.move_dir = move_dir
 
     def check_border(self):
-         if self.move_dir == 'x':
+        if self.move_dir == 'x':
             if self.rect.right >= self.end_pos[0] and self.direction.x == 1: # introduce and end position to the game.
                  self.direction.x = -1 # didnt write X 
                  self.rect.right = self.end_pos[0]
             if self.rect.left <= self.start_pos[0] and self.direction.x == -1:
                 self.direction.x = 1
                 self.rect.left = self.start_pos[0]
-                 
+        else:
+            if self.rect.bottom >= self.end_pos[1] and self.direction.y == 1: # introduce and end position to the game.
+                    self.direction.y = -1 # didnt write X 
+                    self.rect.bottom = self.end_pos[1]
+            if self.rect.top <= self.start_pos[1] and self.direction.y == -1:
+                    self.direction.y = 1
+                    self.rect.top = self.start_pos[1]
 
 
 
